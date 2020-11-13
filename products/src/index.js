2ff2988fc7a5131f7a7263846eada3c17095467e
Haiyang Hong <hong961127@gmail.com>
2020-11-13 07:14:59 -0800

    Finish product microfrontend

--- products/src/index.js
+++ products/src/index.js
@@ -2,9 +2,11 @@ import faker from "faker";
 
 let products = "";
 
+// Generate a list of random product names
 for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
 
-console.log(products);
+// Display product list on screen
+document.querySelector("#dev-products").innerHTML = products;
