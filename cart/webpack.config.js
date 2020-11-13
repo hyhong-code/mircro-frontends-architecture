c708f77ffa6940f83b72b5affa48b90c11993988
Haiyang Hong <hong961127@gmail.com>
2020-11-13 09:31:25 -0800

    Singleton mode module sharing

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -15,8 +15,16 @@ module.exports = {
       exposes: {
         "./CartIndex": "./src/index.js", // Aliasing
       },
-      shared: ["faker"], // Share faker module, so container only choose one to load
+      // shared: {
+      //   faker: {
+      //     singleton: true, // Means we only want to load at most 1 copy, regardless of version
+      //   },
+      // },
+      shared: ["faker"],
+      // Share faker module, so container only choose one to load
       // Shared modules can only be loaded asynchronously
+      // When different version (marjor number) of same module is used between remotes
+      // Host will load multiple scripts
     }),
 
     new HtmlWebpackPlugin({
